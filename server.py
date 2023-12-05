4f7567141741cc8aace82bcfa7524f7724747f51
HyunJinNo <nhj9831@naver.com>
2023-12-05 18:41:03 +0900

    엑셀 파일을 업로드하는 기능과 localization 하는 기능을 분리

--- server.py
+++ server.py
@@ -6,32 +6,33 @@ import os
 app = Flask(__name__)
 eng = matlab.engine.start_matlab()
 
+
 @app.route("/", methods=["GET"])
 def home():
     return "CAU-IoT-Project-Design-2023"
+
 
 @app.route("/is-connected", methods=["GET"])
 def isConnected():
     return "connected"
 
+
 # 클라이언트로부터 센서 데이터 값 받기
-# 해당 기능은 Test 용도
-@app.route("/get-sensor-data", methods=["GET"])
-def getSensorData():
-    if request.method == "GET":
-        x = request.args.get("x")
-        y = request.args.get("y")
-        z = request.args.get("z")
-        
-        return "x: {0}, y: {1}, z: {2}".format(x, y, z)
-        
-@app.route("/indoor-localization", methods=["POST"])
-def localization():
+@app.route("/save-sensor-data", methods=["POST"])
+def saveSensorData():
     if request.method == "POST":
-        file = request.files['file']
+        file = request.files["file"]
         filename = secure_filename(file.name)
-        file.save("./" + filename)
         
+        if os.path.isfile(os.getcwd() + "/" + filename):
+            os.remove(os.getcwd() + "/sensor_data.xls")
+        file.save("./" + filename)
+        return "OK"
+
+
+@app.route("/indoor-localization", methods=["GET"])
+def localization():
+    if request.method == "GET":
         eng.addpath(os.getcwd())
         eng.addpath(os.getcwd())
         result = eng.localization()[0]
@@ -46,6 +47,7 @@ def localization():
             "resultZ": resultZ
             })
 
+
 # TODO: Test 용도로 삭제 예정
 def test():
     eng.addpath(os.getcwd())
@@ -56,6 +58,7 @@ def test():
     print(resultX)
     print(resultY)
     print(resultZ)
+    
     
 if __name__ == "__main__":
     with open("information.txt", encoding="UTF-8") as f:
