04ad60b20a7c9398d74566f7b5befaa98dc19c81
HyunJinNo <nhj9831@naver.com>
2023-12-08 12:42:27 +0900

    클라이언트로부터 rssi 값 받는 함수 작성

--- server.py
+++ server.py
@@ -43,19 +43,39 @@ def localization():
             "resultY": resultY,
             "resultZ": resultZ
             })
-
-
-# TODO: Test 용도로 삭제 예정
-def test():
-    eng.addpath(os.getcwd())
-    result = eng.localization()[0]
-    resultX = result[0]
-    resultY = result[1]
-    resultZ = result[2]
-    print(resultX)
-    print(resultY)
-    print(resultZ)
     
+    
+@app.route("/save-localization-data", methods=["POST"])
+def saveLocalizationData():
+    if request.method == "POST":
+        data = request.get_json()
+
+
+@app.route("/save-localization-data", methods=["POST"])
+def saveLocalizatinoData():
+    if request.method == "POST":
+        data = request.get_json()
+        x = data["x"]
+        y = data["y"]
+        z = data["z"]
+        section = data["section"]
+        with open("data.txt", "a+", encoding="UTF-8") as f:
+            f.write("%f,%f,%f,%d" % (x, y, z, section))
+        return "OK"
+    
+
+@app.route("/save-rssi-data", methods=["GET"])
+def safeRssiData():
+    if request.method == "GET":
+        t1  = request.args.get("t1")
+        r1 = request.args.get("r1")
+        t2  = request.args.get("t2")
+        r2 = request.args.get("r2")
+        t3  = request.args.get("t3")
+        r3 = request.args.get("r3")
+       
+        return "t1: {0}, r1: {1}, t2: {2}, r2: {3}, t3: {4}, r3: {5}".format(t1, r1, t2, r2, t3, r3)
+
     
 if __name__ == "__main__":
     with open("information.txt", encoding="UTF-8") as f:
