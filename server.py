b09d87e233346ba2868e2bfd75771e4862040dc7
limenseop <dmstjq4034@naver.com>
2023-12-11 18:48:00 +0900

    123

--- server.py
+++ server.py
@@ -17,6 +17,19 @@ def isConnected():
     return "connected"
 
 
+@app.route("/save-localization-data", methods=["POST"])
+def saveLocalizatinoData():
+    if request.method == "POST":
+        data = request.get_json()
+        x = data["x"]
+        y = data["y"]
+        z = data["z"]
+        section = data["section"]
+        with open("data.txt", "a+", encoding="UTF-8") as f:
+            f.write("%f,%f,%f,%d\n" % (x, y, z, section))
+        return "OK"
+
+
 # 클라이언트로부터 센서 데이터 값 받기
 @app.route("/save-sensor-data", methods=["POST"])
 def saveSensorData():
@@ -25,6 +38,21 @@ def saveSensorData():
         filename = secure_filename(file.filename)
         file.save("./" + filename)
         return "OK"
+
+
+@app.route("/rssi-measure", methods=["GET"])
+def rssiMeasure():
+    if request.method == "GET":
+        eng.addpath(os.getcwd())
+        result = eng.calculateKalman()[0]
+        ap1 = result[0]
+        ap2 = result[1]
+        ap3 = result[2]
+
+        knnResult = eng.doKNNPrediction(ap1, ap2, ap3)[0]
+        return jsonify({
+            "result": knnResult
+        })
 
 
 @app.route("/indoor-localization", methods=["GET"])
@@ -36,13 +64,13 @@ def localization():
         resultX = result[0]
         resultY = result[1]
         resultZ = result[2]
-        
+
         return jsonify({
             "result": "Section ?",
             "resultX": resultX,
             "resultY": resultY,
             "resultZ": resultZ
-            })
+        })
 
 
 # TODO: Test 용도로 삭제 예정
@@ -55,13 +83,13 @@ def test():
     print(resultX)
     print(resultY)
     print(resultZ)
-    
-    
-if __name__ == "__main__":
-    with open("information.txt", encoding="UTF-8") as f:
-        lines = f.readlines()
-    info = []
-    for line in lines:
-        info.append(line)
 
-    app.run("0.0.0.0", port=info[0])
+
+if __name__ == "__main__":
+    # with open("information.txt", encoding="UTF-8") as f:
+    #     lines = f.readlines()
+    # info = []
+    # for line in lines:
+    #     info.append(line)
+
+    app.run("0.0.0.0", port=8080)
